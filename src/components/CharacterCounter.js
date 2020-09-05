26539c1db1ed2ed93cf914d2e860c6d851d7b61a
André <andrevictor50@gmail.com>
2020-09-04 23:55:49 -0300

    adding basic setup

---
+++ src/components/CharacterCounter.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useRecoilState } from 'recoil';
+
+import { textState } from '../atoms/text';
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+
+function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      {/* <CharacterCount /> */}
+    </div>
+  )
+}
+
+export default CharacterCounter
